f0d0acda9c1a97fef68cbbb292018e2e14e95e27
“mcfee-618” <feipeixuan@163.com>
2020-08-01 17:21:08 +0800

    pdb

---
+++ appendix/README.md
@@ -0,0 +1,3 @@
+
+
+* pdb 调试 https://www.cnblogs.com/xiaohai2003ly/p/8529472.html
--- appendix/src/debug1.py
+++ appendix/src/debug1.py
@@ -5,3 +5,15 @@
 @file: debug1.py.py
 @time: 2020/7/29 下午5:44
 """
+import pdb
+
+def test(x):
+    x+=1
+    print(x)
+
+a = "a string"
+b= "b string"
+pdb.set_trace() #pdb.set_trace()，就可以设置一个断点,进入pdb调试环境，
+print("next step1")
+test(2)
+print("next step2")
